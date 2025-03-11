ff8ab591eec412b34aee7f4c1066f0de4fac9b66
Jeeva RP <jeeva7engineer@gmail.com>
2025-03-11 22:35:02 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{name}")]
+        public IActionResult GetAllStudents(string name)
+        {
+            string[] students = new string[] { "john", "Ram", "ravi", "Emily", "David"};
+            if(students.Contains(name))
+            {
+                return Ok(name);
+            }
+            return Ok(students);
+        }
+    }
+}
