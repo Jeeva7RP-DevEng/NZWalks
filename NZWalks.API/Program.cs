d5b24d20668111d1a7194402e908d8a6448f45e2
Jeeva RP <jeeva7engineer@gmail.com>
2025-03-16 22:28:12 +0530

    Add seeding and CRUD functionality for walks and regions
    - Updated `NZWalksDbContext` to seed `Difficulty` and `Region` entities.
    - Modified `AutoMapperProfiles` to include mappings for new DTOs.
    - Created `WalksController` to handle walk-related HTTP requests.
    - Added `IWalkRepository` interface and `SQLWalkRepository` implementation for data access.
    - Introduced `AddWalkRequestDto` and `WalkDto` for walk data transfer.
    - Updated model snapshot to reflect seeded data.

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 
 var app = builder.Build();
 
